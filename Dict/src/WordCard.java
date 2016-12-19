17f99a847cc6a05dfaf0e680b22bc90a88bd9bfe
hcx <2364684794@qq.com>
2016-12-19 19:03:10 +0800

    alter the interface of the WordCard

--- Dict/src/WordCard.java
+++ Dict/src/WordCard.java
@@ -23,15 +23,16 @@ public class WordCard extends JFrame {
 	private JLabel jlb2 = null;
 	private JTextArea jta = null;
 	private JTextField jtf,jtf2;
-	private JButton jbt1,jbt2,jSavebt,jSendbt;
+	private JButton jbt1,jbt2,jSavebt,jCancelbt;
 	private TitledBorder jtb = new TitledBorder("在此处添加文字");
 	private String path = null;
-	float alpha = 1.0f;
-	String font = "SansSerif";
-	int fontStyle = Font.PLAIN;
-	int fontSize = 15;
-	Color color = Color.BLUE;
-	BufferedImage image = null;
+	private float alpha = 1.0f;
+	private String font = "SansSerif";
+	private int fontStyle = Font.PLAIN;
+	private int fontSize = 15;
+	private Color color = Color.BLUE;
+	private BufferedImage image = null;
+	private Box box = null;
 	
 	WordCard(String word){
 		this.setSize(600,450);
@@ -52,14 +53,18 @@ public class WordCard extends JFrame {
 		
 		ImageIcon imageicon = new ImageIcon(image);
 		
-		jp = new JPanel(new GridLayout(6,1,5,5));
+		//jp = new JPanel(new GridLayout(6,1,5,5));
+		{
+			box = Box.createVerticalBox();
+		}
 		{
 			jlb2 = new JLabel();
 			jlb2.setText("\u6253\u5f00");
 		}
 		{
-			jtf = new JTextField("d://temp/");
-			jtf.setSize(100, 20);
+			jtf = new JTextField(15);
+			jtf.setText("d://temp/");
+			//jtf.setSize(100, 20);
 		}
 		{
 			jbt2 = new JButton("插入图片");
@@ -67,6 +72,7 @@ public class WordCard extends JFrame {
 		{
 			jta = new JTextArea();
 			jta.setBorder(jtb);
+			jta.setPreferredSize(new Dimension(90, 30));
 		}
 		{
 			jbt1 = new JButton("添加文字");
@@ -84,8 +90,9 @@ public class WordCard extends JFrame {
 			
 		}
 		{
-			jSendbt = new JButton("发送图片");
+			jCancelbt = new JButton("取消修改");
 		}
+		
 		JPanel jpl1 = new JPanel();
 		jpl1.add(jlb2);
 		jpl1.add(jtf);
@@ -95,15 +102,19 @@ public class WordCard extends JFrame {
 		jpl2.add(jtf2);
 		jpl2.add(jSavebt);
 		
+		Box b1 = Box.createHorizontalBox();
+		b1.add(jbt1);
+		b1.add(jCancelbt);
 		
-		jp.add(jpl1);
-		jp.add(jta);
-		jp.add(jbt1);
-		jp.add(jpl2);
-		jp.add(jSendbt);
+		box.add(jpl1);
+		box.add(jpl2);
+		box.add(jta);
+		box.add(b1);
+		
+		//jp.add(jSendbt);
 		
 		this.add(jlb1,BorderLayout.WEST);
-		this.add(jp, BorderLayout.CENTER);
+		this.add(box, BorderLayout.CENTER);
 	
 	
 		
@@ -147,6 +158,24 @@ public class WordCard extends JFrame {
 				String des = jtf2.getText();
 				save(des);
 			}
+		});
+		
+		jCancelbt.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				try{
+					
+					image = ImageIO.read(new File(path));
+				}catch(IOException e1){
+					e1.printStackTrace();
+					
+				}
+				ImageIcon imageIcon = new ImageIcon(image);
+			    jlb1.setIcon(imageIcon);
+			}
+			
 		});
 	}
 	
